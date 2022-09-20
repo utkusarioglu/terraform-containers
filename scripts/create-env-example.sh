c7d54d7daf8b24868d280e65ce9278abea521e4f
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-20 09:30:13 +0300

    Feat: Initial commit
    - Update `.gitignore` to include `.env`.
    - Create `create-env-example.sh` and `.vscode/settings.json` for having
      an example for the `.env` values be created on every save, as well as
      having the values be synced to github.
    - Create `Dockerfile.dev`, `DOCKER_README.md` and `devcontainer.json`
      for the creation of the devcontainer. The current image is a simple
      with a few devcontainer features, and some gists copied for DX.

---
+++ scripts/create-env-example.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Creates an "*.example" file for the given ".env" by stripping out
+# the values and the comments.
+
+find . \
+  -maxdepth 1 \
+  -type f \( \( \
+    -iname "*.env" \
+    -or -iname "*.env.*" \) \
+    -and -not -iname "*.example" \) | \
+      while read env_file;
+        do
+          echo "Handling $env_file"
+          awk -F'=' '{print $1}' $env_file | \
+            awk -F'#' '{print $1}' | \
+            awk NF > \
+            "$env_file.example"
+        done
