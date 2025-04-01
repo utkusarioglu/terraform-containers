2e542271b8428b9f880ad591c54bb461d0dde5b3
Utku Sarioglu <utkusarioglu@hotmail.com>
2025-04-01 07:27:00 +0000

    Feat: Upgrade base image

--- src/home/scripts/bootstrap/tf/install-go.sh
+++ src/home/scripts/bootstrap/tf/install-go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eux
+set -euxo pipefail
 bash --version
 
 ARGS=(
@@ -24,8 +24,8 @@ go version
 wget -O - https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
   | tee /etc/apt/sources.list.d/hashicorp.list
-apt update
-apt install terraform
+apt-get update
+apt-get install terraform
 terraform -help
 
 # Install terragrunt
